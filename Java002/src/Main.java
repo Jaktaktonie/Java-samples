9c9b02169bd3f46cbec878de40b1e268aeeb2f54
andrzej <andrzej.czabajski@collegiumwitelona.pl>
2023-04-25 20:51:16 +0200

    print Local Date Time

---
+++ Java002/src/Main.java
@@ -0,0 +1,9 @@
+import java.time.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Witaj Świecie!");
+        LocalDateTime ldt = LocalDateTime.now();
+        System.out.println(ldt);
+    }
+}
