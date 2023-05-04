90bfe326ee62ee73c8fefecd66a92c3a609a43af
andrzej <andrzej.czabajski@collegiumwitelona.pl>
2023-05-04 21:40:36 +0200

    Operacje arytmetyczne + - * /

---
+++ Java006/src/Main.java
@@ -0,0 +1,17 @@
+//Operacje arytmetyczne
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+        int suma = x + y;
+        int roznica = x - y;
+        int iloczyn = x * y;
+        int iloraz = x / y;
+        System.out.println("\nDla liczby 5 i 3\n");
+        System.out.println("Suma: " + suma);
+        System.out.println("Różnica: " + roznica);
+        System.out.println("Iloczyn: " + iloczyn);
+        System.out.println("Iloraz: " + iloraz);
+    }
+}
